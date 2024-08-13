310309f23dfbcc16afb58058b87d1869933381eb
Shreya Jaiswal <shreyaa.we@gmail.com>
2024-08-13 11:10:14 +0530

    all assests added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <div className="app">
+      {/* mount the navbar */}
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import './Navbar.css';
+import { assets } from '../../assets/assets.js'; // Corrected the typo
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="Logo" className='logo'/> 
+      <ul className="navbar-menu">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile-app</li>
+        <li>Contact Us</li>
+      </ul>
+      <div className="navbar-right">
+        <img  src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt =""/>
+
+        </div>
+        <div className="dot">
+          <button>Sign In</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
