7d0613c3948e186189598ca128af25cb64c8b8eb
Shreya Jaiswal <shreyaa.we@gmail.com>
2025-03-16 16:34:48 +0530

    food display done

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
@@ -6,8 +6,33 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = ({ children }) => {   
 
     
+    const [cartItems, setCartItems] = useState({})
+
+    const addToCart =(itemId)=>{
+        if(!cartItems[itemId]){
+            // The curly braces {} create a new object.
+            setCartItems((prev) =>({...prev, [itemId]:1 }))
+        }
+        else{
+            setCartItems((prev) =>({...prev, [itemId]:prev[itemId]+1 }))
+        }
+    }
+    const removeFromCart =(itemId)=>{
+
+        setCartItems((prev) =>({...prev, [itemId]:prev[itemId]-1}))
+    
+    }
+
+    useEffect(()=>{
+        console.log(cartItems)
+    },[cartItems])
+
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     };
 
 // This contextValue object holds the data (food_list) that will be available to all components inside the StoreContextProvider.
