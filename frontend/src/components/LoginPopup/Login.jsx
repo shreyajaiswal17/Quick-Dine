41e98fe690939a701e45abe5ed5bdc17cc7563bb
Shreya Jaiswal <shreyaa.we@gmail.com>
2025-07-02 11:42:39 +0530

    created login page

---
+++ frontend/src/components/LoginPopup/Login.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import './Login.css';
+import { assets } from '../../assets/assets';
+
+const Login = ({setShowLogin}) => {
+    const [currState, setCurrState] = useState("Sign Up")
+  return (
+    <div className='login'>
+      <form className="login-container">
+        <div className="login-title">
+            <h2>{currState}</h2>
+            <img onClick={()=>setShowLogin(false)} src ={assets.cross_icon} alt=""/>
+        </div>
+        <div className="login-input">
+            {currState === "Login"?<></>: <input type ='text' placeholder='Your name' required/>}
+           
+            <input type ='email' placeholder='Your email' required/>
+            <input type ='password' placeholder='Password' required/>
+        </div>
+        <button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
+        <div className="login-popup-condn">
+            <input type ='checkbox' required/>
+            <p>By continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
+        
+        {currState ==='Login'?<p>Creat a new account? <span onClick={()=>setCurrState("Sign Up")}>Click here </span></p>: <p>Already have an account? <span onClick={()=>setCurrState("Login")}>Login here</span></p>
+         }
+        
+      </form>
+    </div>
+  )
+}
+
+export default Login
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import './Navbar.css';
 import { assets } from '../../assets/assets.js'; 
 import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
   const [menu, setMenu] = useState("home")
 
   return (
@@ -28,7 +28,7 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt =""/>
            <div className="dot"></div>
         </div>
-          <button>Sign In</button>
+          <button onClick={()=>setShowLogin(true)}>Sign In</button>
         
       </div>
     </div>
