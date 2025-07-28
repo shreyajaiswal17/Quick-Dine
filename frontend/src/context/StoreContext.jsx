324a9e2ebfaefa50edd1913fce931eed3ca8f4ae
Shreya Jaiswal <shreyaa.we@gmail.com>
2025-07-28 17:27:56 +0530

    cart page donge

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -23,16 +23,29 @@ const StoreContextProvider = ({ children }) => {
     
     }
 
-    useEffect(()=>{
-        console.log(cartItems)
-    },[cartItems])
+    const getTotalCartAmount = () => {
+    let totalAmount = 0;
+
+    for (const item in cartItems) {
+        if (cartItems[item] > 0) {
+            let itemInfo = food_list.find((product) => product._id == item);
+            if (itemInfo) {
+                totalAmount += itemInfo.price * cartItems[item];
+            }
+        }
+    }
+
+    return totalAmount;
+};
+
 
     const contextValue = {
         food_list,
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
     };
 
 // This contextValue object holds the data (food_list) that will be available to all components inside the StoreContextProvider.
