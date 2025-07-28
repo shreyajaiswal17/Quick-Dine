6d97b800c3fdbd6feb21dbcd149ecf09f3e8d2c0
Shreya Jaiswal <shreyaa.we@gmail.com>
2025-07-28 17:34:50 +0530

    ..

--- frontend/src/Pages/Cart/Cart.jsx
+++ frontend/src/Pages/Cart/Cart.jsx
@@ -50,21 +50,21 @@ export const Cart = () => {
 
           <div className="cart-total-details">
             <p>Subtotal</p>
-            <p>{getTotalCartAmount()}</p>
+            <p>${getTotalCartAmount()}</p>
           </div>
 
           <hr />
 
           <div className="cart-total-details">
             <p>Delivery</p>
-            <p>{2}</p>
+            <p>${2}</p>
           </div>
 
           <hr />
 
           <div className="cart-total-details">
             <b>Total</b>
-            <b>{getTotalCartAmount()+2}</b>
+            <b>${getTotalCartAmount()+2}</b>
           </div>
 
           <button>PROCEED TO CHECKOUT</button>
