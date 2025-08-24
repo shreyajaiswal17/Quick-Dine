617d76b74a8cf498115e62ed7ed01846dffb5a02
Shreya Jaiswal <shreyaa.we@gmail.com>
2025-08-24 17:27:11 +0530

    order page for admin panel created

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -87,4 +87,28 @@ const userOrders = async(req,res) =>{
     res.json({success:false, message:"Error fetching orders"});
   }
 }
-export { placeOrder, verifyOrder,userOrders }
+
+// orders from all users - admin panel ke liye
+const listOrders = async(req,res) =>{
+  try {
+    const orders = await orderModel.find();
+    res.json({success:true, data:orders});
+  } catch (error) {
+    console.log(error);
+    res.json({success:false, message:"Error fetching orders"});
+  }
+
+}
+
+// updating order status
+const updateStatus = async(req,res) => {
+  try {
+    await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status});
+    res.json({success:true,message:"Status Updated"})
+  } catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+  }
+}
+
+export { placeOrder, verifyOrder,userOrders, listOrders, updateStatus }
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express"
 import orderModel from "../models/orderModel.js";
-import { placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js";
+import { listOrders, placeOrder, updateStatus, userOrders, verifyOrder } from "../controllers/orderController.js";
 import authMiddleware from "../middlewares/auth.js";
 
 const orderRouter = express.Router()
@@ -9,5 +9,7 @@ orderRouter.post("/place",authMiddleware, placeOrder);
 
 orderRouter.post("/verify", verifyOrder);
 orderRouter.post("/userorders", authMiddleware, userOrders);
+orderRouter.get('/list',listOrders);
+orderRouter.post('/status',updateStatus);
 
 export default orderRouter;
