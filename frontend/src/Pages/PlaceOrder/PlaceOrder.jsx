eb0a60d24bdb166d6783b68d57d7239062eb7ce5
Shreya Jaiswal <shreyaa.we@gmail.com>
2025-08-24 16:14:17 +0530

    frontend completed

--- frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import "./PlaceOrder.css";
 import { StoreContext } from '../../context/StoreContext'
 import axios from 'axios'
@@ -47,6 +47,16 @@ function PlaceOrder() {
     }
   }
 
+
+
+  useEffect(()=>{
+    if(!token){
+      navigate('/cart')
+    }
+    else if(getTotalCartAmount()===0){
+      navigate("/cart")
+    }
+  },[token])
 
 
 // whenever data is updated it executes just for checking
