33e0774195d7a65abeff7fea533e2823c4db1fff
Shreya Jaiswal <shreyaa.we@gmail.com>
2025-08-31 00:36:14 +0530

    .

--- frontend/src/components/foodItem/foodItem.jsx
+++ frontend/src/components/foodItem/foodItem.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import "./foodItem.css";
+import "./FoodItem.css";
 import { assets } from "../../assets/assets";
 import { StoreContext } from "../../context/StoreContext";
 
