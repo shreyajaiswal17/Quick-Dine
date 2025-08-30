77ba07eb19a52e5498e6a244f3552e04443d6bfa
Shreya Jaiswal <shreyaa.we@gmail.com>
2025-08-31 00:44:13 +0530

    Fix FoodItem import using index.js pattern

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
-import { StoreContext } from '../../context/StoreContext'
-import FoodItem from '../FoodItem/FoodItem'
+import { StoreContext } from '../../context/StoreContext.jsx'
+import FoodItem from '../FoodItem'
 
 const  FoodDisplay =({category}) => {
 
